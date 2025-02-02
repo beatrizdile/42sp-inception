4866c5f1205ab03d1cd5e85593b3363fcae1c278
Beatriz Dile <bedos-sa@student.42sp.org.br>
2025-02-01 22:54:07 -0300

    WIP: setting up wordpress

---
+++ srcs/requirements/wordpress/conf/wp_conf.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+rm -rf /var/www/wordpress/wp-config-sample.php
+
+wp config create --allow-root \
+	--path=/var/www/wordpress \
+	--dbname=$DATABASE_NAME \
+	--dbuser=$DATABASE_USER \
+	--dbpass=$DATABASE_USER_PASSWORD \
+	--dbhost="maria_db" \
+	--dbprefix='wp_' \
+	--dbcharset="utf8"
+
+wp core install --allow-root \
+--path=/var/www/wordpress \
+--title="Inception" \
+--url=$DOMAIN_NAME \
+--admin_user=$WP_ROOT_USER \
+--admin_password=$WP_ROOT_PASSWORD \
+--admin_email=$WP_ROOT_EMAIL
+
+wp user create --allow-root	\
+--path=/var/www/wordpress \
+"$WP_USER" "$WP_EMAIL" \
+--user_pass=$WP_PASSWORD \
+--role='author'
+
+# exec php-fpm7.4 -F
+sleep infinity
